c3bc3d569ea3d8c5779ff4574a775522ecc34f54
dj.yang <1769477451@qq.com>
2026-03-31 15:32:56 +0800

    build(pom): 升级所有模块版本至 2.5.0.2

--- xxl-job-admin/deploy.sh
+++ xxl-job-admin/deploy.sh
@@ -2,8 +2,8 @@
 
 
 
-docker build -t  registry.cn-hangzhou.aliyuncs.com/falcon-tools/xxl-job-admin:2.5.0.1 . --network host
-docker push registry.cn-hangzhou.aliyuncs.com/falcon-tools/xxl-job-admin:2.5.0.1
+docker build -t  registry.cn-hangzhou.aliyuncs.com/falcon-tools/xxl-job-admin:2.5.0 . --network host
+docker push registry.cn-hangzhou.aliyuncs.com/falcon-tools/xxl-job-admin:2.5.0
 
 
 #docker tag 3d8f3afdb6cd registry.cn-hangzhou.aliyuncs.com/falcon-tools/postgresql:16
